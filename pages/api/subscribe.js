5b1b395a856bbe8ab68d424cf161688db32fbc97
Akashdeep <akash.kashhyap@gmail.com>
2023-01-30 20:01:35 +0530

    working on cors fixes - 15

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-  const { data } = await axios.get(`https://us21.api.mailchimp.com/3.0/lists/${process.env.LISTIDKEY}/members`, {
+  const { data } = await axios.get(`https://us21.api.mailchimp.com/3.0/lists/${process.env.LISTIDKEY}`, {
     headers: {
       'Authorization': `auth ${process.env.MAILCHIMP_API_KEY}`
     }
