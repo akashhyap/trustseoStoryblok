8a41d18e2d579f8cb7ce0d206e0829cf158c58ea
Akashdeep <akash.kashhyap@gmail.com>
2023-01-31 10:49:33 +0530

    mailchimp api integration and contact form UI design

--- components/Form.js
+++ components/Form.js
@@ -6,76 +6,76 @@ import axios from "axios";
 const Contact = ({ blok }) => {
   const [formData, setFormData] = useState({});
 
-//   console.log('blok', blok);
+  //   console.log('blok', blok);
   useEffect(() => {
     axios
-      .get(`https://api.storyblok.com/v1/cdn/stories/${process.env.NEXT_PUBLIC_STORYID}?token=${process.env.NEXT_PUBLIC_SITETOKKEN}`)
-      .then(response => {
-        const mailchimpApiKey  = process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY;
-        const listId  = response.data.story.content._uid;
+      .get(
+        `https://api.storyblok.com/v1/cdn/stories/${process.env.NEXT_PUBLIC_STORYID}?token=${process.env.NEXT_PUBLIC_SITETOKKEN}`
+      )
+      .then((response) => {
+        const mailchimpApiKey = process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY;
+        const listId = response.data.story.content._uid;
 
         // console.log('mailcresponsehimpApiKey',  mailchimpApiKey);
-     
-       
+
         setFormData({ listId, mailchimpApiKey });
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   }, []);
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('test', formData);
+    console.log("test", formData);
     const { email, name, phone, message } = e.target.elements;
     try {
       const subscriber = {
         email_address: email.value,
-        status: 'subscribed',
+        status: "subscribed",
         merge_fields: {
           FNAME: name.value,
           PHONE: phone.value,
-          MESSAGE: message.value
-        }
+          MESSAGE: message.value,
+        },
       };
       const res = await axios.post(
         `https://trustseo-storyblok.vercel.app/api/subscribe`,
-        {
-          subscriber,
-          listId: formData.listId,
-          mailchimpApiKey: process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY
-        }
+        subscriber
       );
-  
+      // return res.data;
       console.log(res.data);
     } catch (err) {
       console.error(err);
     }
   };
 
-
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col w-full md:w-7/12 m-auto text-md md:text-lg">
-      <label>
+    <div className="md:w-7/12 m-auto mt-8">
+      <form
+      onSubmit={handleSubmit}
+      className="flex flex-col w-full md:w-7/12 m-auto text-md md:text-lg bg-white px-4 py-5 sm:p-6 border shadow sm:rounded-md"
+    >
+      <label className="mb-3">
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" required className="mt-1 block w-full rounded-md border py-1 px-3 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
       </label>
-      <label>
+      <label className="mb-3">
         Email:
-        <input type="email" name="email" />
+        <input type="email" name="email" required className="mt-1 block w-full rounded-md border py-1 px-3 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
       </label>
-      <label>
+      <label className="mb-3">
         Phone:
-        <input type="tel" name="phone" />
+        <input type="tel" name="phone" className="mt-1 block w-full rounded-md border py-1 px-3 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"/>
       </label>
-      <label>
+      <label className="mb-3">
         Message:
-        <textarea name="message" />
+        <textarea name="message" rows="3" className="mt-1 block w-full rounded-md border py-1 px-3 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
       </label>
-      <button type="submit">Submit</button>
+      <button type="submit" className="capitalize border border-logo-red bg-logo-red hover:bg-transparent text-white hover:text-logo-red transition-colors font-poppins text-sm px-5 py-3 mt-2 rounded-md ">Submit</button>
     </form>
+    </div>
   );
-
 };
 
 export default Contact;
--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,10 +1,11 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-  const { data } = await axios.get(`https://us21.api.mailchimp.com/3.0/lists/${process.env.LISTIDKEY}/members`, {
+  const subscriber = req.body;
+  const { data } = await axios.post(`https://us21.api.mailchimp.com/3.0/lists/${process.env.LISTIDKEY}/members`, subscriber, {
     headers: {
       'Authorization': `auth ${process.env.MAILCHIMP_API_KEY}`
     }
   });
- res.json(data);
+  res.json(data);
 };
