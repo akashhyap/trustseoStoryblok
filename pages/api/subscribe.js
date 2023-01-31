fa7eb81560bcd6851e4b1bdc04d90b08c6431687
Akashdeep <akash.kashhyap@gmail.com>
2023-01-31 13:53:05 +0530

    mailchimp api integration and contact form error handling - 2

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -2,11 +2,26 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
   const subscriber = req.body;
-  const { data } = await axios.post(`https://us21.api.mailchimp.com/3.0/lists/${process.env.LISTIDKEY}/members`, subscriber, {
-    headers: {
-      'Authorization': `auth ${process.env.MAILCHIMP_API_KEY}`
+
+  try {
+    const { data } = await axios.get(`https://us21.api.mailchimp.com/3.0/lists/${process.env.LISTIDKEY}/members?email=${subscriber.email_address}`, {
+      headers: {
+        'Authorization': `auth ${process.env.MAILCHIMP_API_KEY}`
+      }
+    });
+
+    if (data.members.length > 0 && data.members[0].email_address === subscriber.email_address) {
+      return res.status(400).json({ error: 'Email already exists' });
     }
-  });
-  console.log('subscriber',subscriber);
-  res.json(data);
+
+    const { data: result } = await axios.post(`https://us21.api.mailchimp.com/3.0/lists/${process.env.LISTIDKEY}/members`, subscriber, {
+      headers: {
+        'Authorization': `auth ${process.env.MAILCHIMP_API_KEY}`
+      }
+    });
+
+    res.json(result);
+  } catch (error) {
+    res.status(error.response.status).json({ error: error.response.data });
+  }
 };
